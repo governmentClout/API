69e38d15657a4d184c60b7a665804f246198a605
Seyi Onifade <xyluz@ymail.com>
2018-10-11 22:14:55 +0100

    content ype

--- index.js
+++ index.js
@@ -10,6 +10,7 @@ const fs = require('fs');
 const handlers = require('./lib/handlers');
 const helpers = require('./lib/helpers');
 const dbconnect = require('./lib/db_connect');
+const cors = require('cors');
 
 
 const httpServer = http.createServer((req,res)=>{
@@ -25,6 +26,30 @@ httpServer.listen(config.httpPort, ()=>{
 });
 
 var unifiedServer = (req,res)=>{	
+
+	// req.writeHeader("Access-Control-Allow-Origin","*");
+	// req.writeHeader("Access-Control-Allow-Headers","Origin, X-Requested-With, Content-Type, Accept");
+
+	  const head = {
+	    'Access-Control-Allow-Origin': '*',
+	    'Access-Control-Allow-Methods': 'OPTIONS, POST, GET, PUT',
+	    'Access-Control-Max-Age': 2592000, 
+	  };
+
+	  if (req.method === 'OPTIONS') {
+	    res.writeHead(204, head);
+	    res.end();
+	    return;
+	  }
+
+	  if (['GET', 'POST', 'PUT'].indexOf(req.method) > -1) {
+	    res.writeHead(200, head);
+	    res.end('Hello World');
+	    return;
+	  }
+
+	  	res.writeHead(405, head);
+  		res.end(`${req.method} is not allowed for the request.`);
 	
 	const parsedUrl = url.parse(req.url,true);
 	
