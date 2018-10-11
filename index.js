11c3d39cb8859c9d1608fc5682d322a22037bd92
Seyi Onifade <xyluz@ymail.com>
2018-10-11 18:25:22 +0100

    remove header

--- index.js
+++ index.js
@@ -25,9 +25,6 @@ httpServer.listen(config.httpPort, ()=>{
 });
 
 var unifiedServer = (req,res)=>{
-
-	res.header("Access-Control-Allow-Origin", "*");
-  	res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
 	
 	const parsedUrl = url.parse(req.url,true);
 	
