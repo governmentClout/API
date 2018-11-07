31d7319de6bb2eb6cf9ea810818cbac19f55a6d5
Seyi Onifade <xyluz@ymail.com>
2018-11-07 19:04:32 +0100

    install nodemailer

---
+++ components/petitions.js
@@ -0,0 +1,31 @@
+
+
+const _db = require('./../lib/migrations');
+const helpers = require('./../lib/helpers');
+const uuidV1 = require('uuid/v4');
+const config = require('./../lib/config');
+const mysql = require('mysql');
+const tokens = require('./../lib/tokenization');
+
+const con = mysql.createConnection({
+
+  host: config.db_host,
+  user: config.db_username,
+  password: config.db_password,
+  database: config.db_name
+
+});
+
+
+let petitons = {};
+
+
+petitons.options = (data,callback)=>{
+
+	callback(200,data.headers);
+	
+}
+
+
+
+module.exports = users;
---
+++ components/polls.js
@@ -0,0 +1,30 @@
+
+
+const _db = require('./../lib/migrations');
+const helpers = require('./../lib/helpers');
+const uuidV1 = require('uuid/v4');
+const config = require('./../lib/config');
+const mysql = require('mysql');
+
+const con = mysql.createConnection({
+
+  host: config.db_host,
+  user: config.db_username,
+  password: config.db_password,
+  database: config.db_name
+
+});
+
+
+let petitons = {};
+
+
+petitons.options = (data,callback)=>{
+
+	callback(200,data.headers);
+	
+}
+
+
+
+module.exports = users;
