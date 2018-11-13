8cf3b179f0f9ccac07f287bb8a74e0bad2c4244b
Seyi Onifade <xyluz@ymail.com>
2018-11-13 22:23:45 +0100

    re-deploy

--- components/friends.js
+++ components/friends.js
@@ -61,6 +61,69 @@ friends.get = (data,callback)=>{
 
 				){
 
+					if(queryObject && queryObject.user != uuidHeader && param == 'friends'){
+
+					let finalresult = [];
+
+						async.waterfall([
+						    function(callback) {
+
+						    	let sqlGetFriends = "SELECT * FROM friends WHERE user='"+user+"' AND status=2";
+
+						    	con.query(sql,(err,result)=>{
+						    			
+						    			if(!err && result.length > 0){
+						    				callback(null,result);
+						    			}else{
+						    				callback(null,[]);
+						    			}
+										
+
+									});
+						    	
+						    
+						    },
+						    function(arg, callback) {
+						    	
+						    	if(arg.length > 0){
+
+						    		let result = [];
+							    	var pending = arg.length;
+
+							    	for(let i=0; i<arg.length; i++) {
+							    		// console.log(arg[i].uuid);
+							    	  con.query("SELECT * FROM profiles WHERE uuid='"+arg[i].uuid+"'",(err, result)=>{
+							    	 		
+							    	 		
+								            finalresult.splice(i,0,result);
+								            
+
+								            if( 0 === --pending ) {
+
+								               	callback(null,finalresult);
+
+								            }
+
+								        });
+							    	}
+
+						    	}else{
+						    		callback(null, []);
+						    	}
+						    	
+
+						        
+						    }
+						], function (err, result) {
+							
+							callback(200,{'friends':result});
+
+						});
+
+				}else{
+					callback(403,{'Error':'Access Denied'});
+				}
+				
 
 
 					if(param && param == 'friends'){
@@ -293,6 +356,69 @@ friends.get = (data,callback)=>{
 
 								            }
 
+	if(queryObject && queryObject.user != uuidHeader && param == 'friends'){
+
+					let finalresult = [];
+
+						async.waterfall([
+						    function(callback) {
+
+						    	let sqlGetFriends = "SELECT * FROM friends WHERE user='"+user+"' AND status=2";
+
+						    	con.query(sql,(err,result)=>{
+						    			
+						    			if(!err && result.length > 0){
+						    				callback(null,result);
+						    			}else{
+						    				callback(null,[]);
+						    			}
+										
+
+									});
+						    	
+						    
+						    },
+						    function(arg, callback) {
+						    	
+						    	if(arg.length > 0){
+
+						    		let result = [];
+							    	var pending = arg.length;
+
+							    	for(let i=0; i<arg.length; i++) {
+							    		// console.log(arg[i].uuid);
+							    	  con.query("SELECT * FROM profiles WHERE uuid='"+arg[i].uuid+"'",(err, result)=>{
+							    	 		
+							    	 		
+								            finalresult.splice(i,0,result);
+								            
+
+								            if( 0 === --pending ) {
+
+								               	callback(null,finalresult);
+
+								            }
+
+								        });
+							    	}
+
+						    	}else{
+						    		callback(null, []);
+						    	}
+						    	
+
+						        
+						    }
+						], function (err, result) {
+							
+							callback(200,{'friends':result});
+
+						});
+
+				}else{
+					callback(403,{'Error':'Access Denied'});
+				}
+				
 								        });
 							    	}
 
--- components/login.js
+++ components/login.js
@@ -1,4 +1,3 @@
-
 const helpers = require('./../lib/helpers');
 const uuidV1 = require('uuid/v4');
 const config = require('./../lib/config');
@@ -28,62 +27,47 @@ login.post = (data,callback)=>{
 //check token
 	let email = typeof(data.payload.email) == 'string' && data.payload.email.trim().length >= 10 ? data.payload.email.trim() : false;
 	let password = typeof(data.payload.password) == 'string' && data.payload.password.trim().length > 0 ? data.payload.password.trim() : false;
-	let provider = typeof(data.payload.provider) == 'string' && data.payload.provider.trim().length > 0 ? data.payload.provider.trim() : false;
 
-	if(email && password && provider){
+	if(email && password){
 
-		if(provider == 'email'){
+		let hashedPassword = helpers.hash(password);
 
-			let hashedPassword = helpers.hash(password);
-
-			const login = "SELECT * FROM users WHERE email='" + email + "' AND password='" + hashedPassword + "'";
-			
-
-			con.query(login,  (err,result) => {
-	
-
-				if(!err && result.length > 0){
-
-					let verifyToken = "SELECT token FROM tokens WHERE uuid='" + result[0].uuid + "' LIMIT 1; SELECT * FROM profiles where uuid='" + result[0].uuid 	+"'";
-					
-					con.query(verifyToken, (err,tokenResult)=>{
-
-						if(
-							!err && 
-							tokenResult &&
-							tokenResult.length > 0 
-						){
-							
-						let finalReqult = Object.assign(...result, ...tokenResult[0],...tokenResult[1]);
-							callback(200,{'user':finalReqult});
-
-						}else{
-							callback(500,{'Error':'Login failed, token Not found or expired'});
-						}
-
-					});
-					
-				}else{
-					console.log(err);
-					callback(404,{'Error':'User not found'});
-				}
-
-			});
-
-
-		}
-
-		if(
-			provider == 'twitter' || 
-			provider == 'facebook' ||
-			provider == 'google' ||
-			provider == 'linkedin'
-			){
-
-			//this might not be needed
-
-		}
+		const login = "SELECT uuid,email,phone,dob FROM users WHERE email='" + email + "' AND password='" + hashedPassword + "'";
 		
+
+		con.query(login,  (err,result) => {
+
+			if(!err && result.length > 0){
+
+				let verifyToken = "SELECT token FROM tokens WHERE uuid='" + result[0].uuid + "' LIMIT 1; SELECT * FROM profiles where uuid='" + result[0].uuid 	+"'";
+				
+				con.query(verifyToken, (err,tokenResult)=>{
+
+					console.log('Token: ' + tokenResult);
+
+					if(
+						!err && 
+						tokenResult &&
+						tokenResult.length > 0 
+					){
+						
+					let finalReqult = Object.assign(...result, ...tokenResult[0],...tokenResult[1]);
+						callback(200,{'user':finalReqult});
+
+					}else{
+						callback(500,{'Error':'Login failed, token Not found or expired'});
+					}
+
+				});
+				
+
+			}else{
+				console.log(err);
+				callback(404,{'Error':'User not found'});
+			}
+
+		});
+
 	}
 }
 
