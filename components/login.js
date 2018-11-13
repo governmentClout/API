61d4a2d0a4798702d8988efda903abaaa12d375c
Seyi Onifade <xyluz@ymail.com>
2018-11-13 07:48:41 +0100

    pending login restructure

--- components/login.js
+++ components/login.js
@@ -24,57 +24,66 @@ login.options = (data,callback)=>{
 }
 
 login.post = (data,callback)=>{
-console.log('poinst 1');
+
 //check token
 	let email = typeof(data.payload.email) == 'string' && data.payload.email.trim().length >= 10 ? data.payload.email.trim() : false;
 	let password = typeof(data.payload.password) == 'string' && data.payload.password.trim().length > 0 ? data.payload.password.trim() : false;
+	let provider = typeof(data.payload.provider) == 'string' && data.payload.provider.trim().length > 0 ? data.payload.provider.trim() : false;
 
-	if(email && password){
-console.log('poinst 2');
+	if(email && password && provider){
 
-		let hashedPassword = helpers.hash(password);
+		if(provider == 'email'){
 
-		const login = "SELECT uuid,email,phone,dob FROM users WHERE email='" + email + "' AND password='" + hashedPassword + "'";
+			let hashedPassword = helpers.hash(password);
+
+			const login = "SELECT * FROM users WHERE email='" + email + "' AND password='" + hashedPassword + "'";
+			
+
+			con.query(login,  (err,result) => {
+	
+
+				if(!err && result.length > 0){
+
+					let verifyToken = "SELECT token FROM tokens WHERE uuid='" + result[0].uuid + "' LIMIT 1; SELECT * FROM profiles where uuid='" + result[0].uuid 	+"'";
+					
+					con.query(verifyToken, (err,tokenResult)=>{
+
+						if(
+							!err && 
+							tokenResult &&
+							tokenResult.length > 0 
+						){
+							
+						let finalReqult = Object.assign(...result, ...tokenResult[0],...tokenResult[1]);
+							callback(200,{'user':finalReqult});
+
+						}else{
+							callback(500,{'Error':'Login failed, token Not found or expired'});
+						}
+
+					});
+					
+				}else{
+					console.log(err);
+					callback(404,{'Error':'User not found'});
+				}
+
+			});
+
+
+		}
+
+		if(
+			provider == 'twitter' || 
+			provider == 'facebook' ||
+			provider == 'google' ||
+			provider == 'linkedin'
+			){
+
+			//this might not be needed
+
+		}
 		
-
-		con.query(login,  (err,result) => {
-console.log('poinst 3');
-
-			if(!err && result.length > 0){
-
-				// console.log('fields ' + fields);
-
-				let verifyToken = "SELECT token FROM tokens WHERE uuid='" + result[0].uuid + "' LIMIT 1; SELECT * FROM profiles where uuid='" + result[0].uuid 	+"'";
-				
-				con.query(verifyToken, (err,tokenResult)=>{
-console.log('poinst 4');
-
-					console.log('Token: ' + tokenResult);
-
-					if(
-						!err && 
-						tokenResult &&
-						tokenResult.length > 0 
-					){
-						
-					let finalReqult = Object.assign(...result, ...tokenResult[0],...tokenResult[1]);
-						callback(200,{'user':finalReqult});
-
-					}else{
-						callback(500,{'Error':'Login failed, token Not found or expired'});
-					}
-
-				});
-				//get token, and sent to them. using uuid
-				
-
-			}else{
-				console.log(err);
-				callback(404,{'Error':'User not found'});
-			}
-
-		});
-
 	}
 }
 
