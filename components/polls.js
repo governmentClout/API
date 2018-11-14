64b4c9b570a70f1b400a8abf24d8211ab19b00ea
Seyi Onifade <xyluz@ymail.com>
2018-11-14 14:31:17 +0100

    poll response submission

--- components/polls.js
+++ components/polls.js
@@ -34,16 +34,15 @@ polls.post = (data,callback)=>{
 	let user = typeof(uuidHeader) == 'string' && uuidHeader.trim().length > 0 ? uuidHeader.trim() : false;
 	let token = typeof(tokenHeader) == 'string' && tokenHeader.trim().length > 0 ? tokenHeader.trim() : false;
 
-	let ex = new Date().toISOString().slice(0, 19).replace('T', ' ');
-
-	let param = typeof(data.payload.param) == 'string' && data.payload.param.trim().length > 0 ? data.payload.param.trim() : false;
+	let param = typeof(data.param) == 'string' && data.param.trim().length > 0 ? data.param.trim() : false;
 	let sector = typeof(data.payload.sector) == 'string' && data.payload.sector.trim().length > 0 ? data.payload.sector.trim() : false;
 	let opinion = typeof(data.payload.opinion) == 'string' && data.payload.opinion.trim().length > 0 ? data.payload.opinion.trim() : false;
-	let expire_at = typeof(data.payload.expire_at) == 'string' && data.payload.expire_at.trim().length > 0 ? data.payload.expire_at.trim() : ex;
+	let expire_at = typeof(data.payload.expire_at) == 'string' && data.payload.expire_at.trim().length > 0 ? data.payload.expire_at.trim() : '3014-01-01 00:00:00';
 	let response_limit = typeof(data.payload.response_limit) == 'string' && data.payload.response_limit.trim().length > 0 ? data.payload.response_limit.trim() : '1000';
-	let poll = typeof(data.payload.poll) == 'string' && data.payload.poll.trim().length > 0 ? data.payload.poll.trim() : false;
-	let status = typeof(data.payload.status) == 'number' && data.payload.status != 0 ? data.payload.status : false;
+
 	let queryObject = Object.keys(data.queryStringObject).length > 0 && typeof(data.queryStringObject) == 'object' ? data.queryStringObject : false;
+
+	let poll = typeof(data.payload.poll) == 'string' && data.payload.poll.trim().length > 0 ? data.payload.poll.trim() : false;
 
 	let uuid = uuidV1();
 
@@ -110,14 +109,14 @@ polls.post = (data,callback)=>{
 					//check that user has not already responded
 					//response status: 1 - true/agree/yes 2 - no/disagree/false 3 - undecided
 
-					let checkResponse = "SELECT count(*) FROM polls_response WHERE user='"+user+"' and poll='" +poll+ "'";
+					let checkResponse = "SELECT count(*) as count FROM polls_response WHERE user='"+user+"' and poll='" +poll+ "'";
 
 					con.query(checkResponse,(err,result)=>{
-
-						if(!err && result.length < 1){
+						// console.log(result[0].count);
+						if(!err && result[0].count == 0){
 
 							
-							let sqlResponse = "INSERT polls_response (user,poll,status) VALUES ('"+user+"','"+poll+"','"+status+"')";
+							let sqlResponse = "INSERT polls_response (user,poll,status) VALUES ('"+user+"','"+poll+"',1)";
 
 							con.query(sqlResponse,(err,result)=>{
 
