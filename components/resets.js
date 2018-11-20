a4385e6eb6100cfb23a5bce7659dc5bef816d3bc
Seyi Onifade <xyluz@ymail.com>
2018-11-20 20:26:40 +0100

    password reset

--- components/resets.js
+++ components/resets.js
@@ -4,6 +4,7 @@ const uuidV1 = require('uuid/v4');
 const config = require('./../lib/config');
 const mysql = require('mysql');
 const tokens = require('./../lib/tokenization');
+const mailer = require('./mailer');
 
 
 
@@ -26,17 +27,75 @@ resets.options = (data,callback)=>{
 }
 
 resets.post = (data,callback)=>{
-	//check that reset link has not expired
+	
+	//get reset email
+	//collect user email, check if user email exists, 
+	//generate password reset code, 
+	//send password reset code to user
+
+	let email = typeof(data.payload.email) == 'string' && data.payload.email.trim().length > 0 ? data.payload.email.trim() : false;
+
+	if(email){
+
+		//check that this email exists in the database
+
+		let checkEmail = "SELECT * FROM users WHERE email='"+email+"'";
+
+		con.query(checkEmail,(err,result)=>{
+
+			if(!err && result.length > 0){
+
+				//generate password reset code
+				let resetCode = tokens.resetCode(email);
+				let uuid = uuidV1();
+
+					//enter the code in the database
+					let resetSQL = "INSERT INTO password_reset (uuid,email,code,status) VALUES('"+uuid+"','"+email+"','"+resetCode+"','0')";
+
+					con.query(resetSQL,(err,result)=>{
+
+						if(!err){
+
+							mailer.sendByEmail([
+							'email':email,
+		   					'subject':'Password Reset Code',
+		   					'message':'Your password reset code is ' + resetCode
+							]);
+
+						}else{
+							console.log(err);
+							callback(500,{'Error','Something went wrong, reset code not sent'});
+						}
+
+					});
+
+				callback(200,{'Success':'Password reset code has been sent'});
+
+			}else{
+				callback(404,{'Error':'Email not found'});
+			}
+		})
+
+	}else{
+		callback(400,{'Error':'Email is a required field'});
+	}
+
+
+
 }
 
 
 resets.put = (data,callback)=>{
-	//update password
+	
+
+
 }
 
 resets.get = (data,callback)=>{
-	//get reset email
-	
+
+
+
+
 }
 
 
