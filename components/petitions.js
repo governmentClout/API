b2dd25c836ff51a11559fc001f3dd9b8f463cacc
Seyi Onifade <xyluz@ymail.com>
2018-12-16 05:58:59 -0800

    getting started with petition

--- components/petitions.js
+++ components/petitions.js
@@ -26,6 +26,146 @@ petitons.options = (data,callback)=>{
 	
 }
 
+petitons.post = (data,callback)=>{
+
+	//create polls
+	//respond to polls
+	let tokenHeader = data.headers.token;
+	let uuidHeader = data.headers.uuid; 
+	let user = typeof(uuidHeader) == 'string' && uuidHeader.trim().length > 0 ? uuidHeader.trim() : false;
+	let token = typeof(tokenHeader) == 'string' && tokenHeader.trim().length > 0 ? tokenHeader.trim() : false;
+
+	let param = typeof(data.param) == 'string' && data.param.trim().length > 0 ? data.param.trim() : false;
+	let sector = typeof(data.payload.sector) == 'string' && data.payload.sector.trim().length > 0 ? data.payload.sector.trim() : false;
+	let opinion = typeof(data.payload.opinion) == 'string' && data.payload.opinion.trim().length > 0 ? data.payload.opinion.trim() : false;
+	let expire_at = typeof(data.payload.expire_at) == 'string' && data.payload.expire_at.trim().length > 0 ? data.payload.expire_at.trim() : '2054-01-01 00:00:00';
+	let response_limit = typeof(data.payload.response_limit) == 'string' && data.payload.response_limit.trim().length > 0 ? data.payload.response_limit.trim() : '1000';
+	let status = typeof(data.payload.status) == 'string' && data.payload.status.trim().length > 0 ? data.payload.status.trim() : '1';
+
+	let queryObject = Object.keys(data.queryStringObject).length > 0 && typeof(data.queryStringObject) == 'object' ? data.queryStringObject : false;
+
+	let poll = typeof(data.payload.poll) == 'string' && data.payload.poll.trim().length > 0 ? data.payload.poll.trim() : false;
+
+	let uuid = uuidV1();
+
+
+	if( 
+		token && 
+		uuidHeader 
+
+		){
+
+		let verifyToken = "SELECT token FROM " + config.db_name + ".tokens WHERE uuid='" + user + "'";
+
+		con.query(verifyToken, (err,result)=>{
+				
+			if(
+				!err && 
+				result[0] && 
+				result[0].token == token 
+
+				){
+
+				if( !param && opinion && sector ){
+
+					//create new poll
+					//user is the uuid of the creator
+				
+					let sqlCreatePoll = "INSERT INTO polls (uuid,sector,opinion,expire_at,response_limit,created_by) VALUES ('"+uuid+"','"+sector+"','"+opinion+"','"+expire_at+"','"+response_limit+"','" + user + "')";
+
+					con.query(sqlCreatePoll,(err,result)=>{
+
+						if(!err && result){
+
+							callback(200,{'Success':'Poll Created'});
+
+						}else{
+							console.log(err);
+							callback(500,{'Error':err});
+						}
+
+					});
+
+				}
+
+				if(!param && (!opinion || !sector)){
+
+					let errorObject = [];
+					if(!param){
+						errorObject.push('No Parameter set');
+					}
+					if(!opinion){
+						errorObject.push('Opinion required');
+					}
+					if(!sector){
+						errorObject.push('sector is required');
+					}
+					console.log(errorObject);
+					callback(400,{'Error':errorObject});
+
+				}
+
+				if(param && param == 'response'){
+					// respond to a poll
+					//user is the uuid of the responder, poll is the poll
+					//check that user has not already responded
+					//response status: 1 - true/agree/yes 2 - no/disagree/false 3 - undecided
+
+					let checkResponse = "SELECT count(*) as count FROM polls_response WHERE user='"+user+"' and poll='" +poll+ "'";
+
+					con.query(checkResponse,(err,result)=>{
+						// console.log(result[0].count);
+						if(!err && result[0].count == 0){
+
+							
+							let sqlResponse = "INSERT polls_response (user,poll,status) VALUES ('"+user+"','"+poll+"','" +status+ "')";
+
+							con.query(sqlResponse,(err,result)=>{
+
+								if(!err && result){
+
+									callback(200,{'Success':'response submitted'});
+
+								}else{
+									callback(500,{'Error':err});
+								}
+
+							});
+
+
+
+						}else{
+							callback(400,{'Error':'User already responded to poll'});
+						}
+
+					});
+
+				}
+			}else{
+				console.log(err);
+				callback(400,{'Error':'Token Mismatch or expired'});
+			
+			}
+		});
+
+	}else{
+
+		let errorObject = [];
+
+		if(!token){
+			errorObject.push('Token you supplied is not valid or has expired');
+		}
+		if(!uuidHeader){
+			errorObject.push('uuid in the header not found');
+		}
+
+		callback(400,{'Error':errorObject});
+
+	}
+	
+}
+
+
 
 
 module.exports = petitons;
