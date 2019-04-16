af29b9ecde85a4c4a86eda6b051583bbaeb4815a
Seyi Onifade <xyluz@ymail.com>
2019-04-17 00:48:43 +0100

    refactoring everything is basically broken

---
+++ database/db.js
@@ -0,0 +1,19 @@
+// Mainly to check if mysql is installed and connected correctly
+
+const mysql = require('mysql');
+const config = require('./config.js');
+
+
+
+let db = mysql.createConnection({
+
+  host: config.db_host,
+  user: config.db_username,
+  password: config.db_password,
+  database: config.db_name,
+  multipleStatements: true
+
+});
+
+
+module.exports = db;
