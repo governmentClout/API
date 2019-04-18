e9452b668f8f44c868e9134b10322a7777f5bf90
Seyi Onifade <xyluz@ymail.com>
2019-04-18 19:46:42 +0100

    get all pending friend requests

--- components/friendrequests.js
+++ components/friendrequests.js
@@ -140,9 +140,214 @@ friendrequests.post = (data,callback)=>{
         }
 }
 
-friendrequests.get = (data,callback)=>{
-    callback(200,{'Success':'Friend Requests get endpoint'});
+/**
+ * @api {get} /friendrequests/:uuid Send New Friend Request 
+ *
+ * @apiName getFriendRequest
+ * @apiGroup FriendRequests
+ * @apiHeader {String} uuid Authorization UUID .
+ * @apiHeader {String} Token Authorization Token.
+ * @apiDescription The endpoint send friend request
+ * @apiParam {String} uuid uuid of the user  
+ *
+ *@apiSuccessExample Success-Response:
+ *HTTP/1.1 200 OK
+{
+    "Success": "Friend Request Sent"
+}
+ *@apiErrorExample Error-Response:
+ *HTTP/1.1 405 Bad Request
+{
+    []
+}
 
+ */
+// let request_sender = typeof(data.payload.request_sender) == 'string' && data.payload.request_sender.trim().length > 0 ? data.payload.request_sender.trim() : false;
+// let request_receiver
+friendrequests.get = (data,callback)=>{
+    
+    let uuid = typeof(data.headers.uuid) == 'string' && data.headers.uuid.trim().length > 0 ? data.headers.uuid.trim() : false;
+    let token = typeof(data.headers.token) == 'string' && data.headers.token.trim().length > 0 ? data.headers.token.trim() : false;
+
+    let param = typeof(data.param) == 'string' && data.param.trim().length > 0 ? data.param.trim() : false;
+
+    let queryObject = Object.keys(data.queryStringObject).length > 0 && typeof(data.queryStringObject) == 'object' ? data.queryStringObject : false;
+
+    let page = typeof(data.queryStringObject.page) == 'string'  ? data.queryStringObject.page : '1'; 
+	let limit = typeof(data.queryStringObject.limit) == 'string' ? data.queryStringObject.limit : '10';
+	let sort = typeof(data.queryStringObject.sort) == 'string' && data.queryStringObject.sort.trim().length > 0 && (data.queryStringObject.sort.trim() == 'ASC' || 'DESC') ? data.queryStringObject.sort.trim() : 'DESC';
+
+    if( 
+		token && 
+		uuid
+
+		){
+            let verifyToken = "SELECT token FROM " + config.db_name + ".tokens WHERE uuid='" + uuid + "'";
+
+            con.query(verifyToken, (err,result)=>{
+                
+                if(
+                    !err && 
+                    result[0] && 
+                    result[0].token == token 
+
+                    ){
+
+                        async.waterfall([
+						    function(callback) {
+
+						    	let sqlGetFriends = "SELECT * FROM friendrequests WHERE request_sender='"+param+"'";
+
+						    	if(sort){
+						    		sqlGetFriends += " ORDER BY id " + sort;
+	 					    	}
+
+						    	if(limit){
+						    		sqlGetFriends += " LIMIT " + limit;
+						    	}
+
+						    	if(page){
+						    		
+						    		let skip = page == '1' ? 0 : page * limit;
+						    		sqlGetFriends += " OFFSET " + skip;
+
+						    	}
+
+						    	con.query(sqlGetFriends,(err,result)=>{
+						    				
+						    			if(!err && result.length > 0){
+						    				callback(null,result);
+						    			}else{
+						    				callback(null,[]);
+						    			}
+										
+
+									});
+						    	
+						    
+						    },
+						    function(arg, callback) {
+						    	
+						    	if(arg.length > 0){
+
+						    		let fromResult = [];
+							    	var pending = arg.length;
+							    	// console.log(arg);
+							    	for(let i=0; i<arg.length; i++) {
+							    		
+							    	  con.query("SELECT * FROM profiles WHERE uuid='"+arg[i].request_sender+"'; SELECT * FROM users WHERE uuid='"+arg[i].request_sender+"'",(err, result)=>{
+							    	 		
+							    	 		
+                                        fromResult.splice(i,0,{'user':result[1],'profile':result[0]});
+								            
+								            if( 0 === --pending ) {
+
+								               	callback(null,fromResult);
+
+								            }
+
+								        });
+							    	}
+
+						    	}else{
+						    		callback(null, []);
+						    	}						    	
+
+						        
+						    }, function(arg, callback) {
+						    	
+                            let sqlGetFriends = "SELECT * FROM friendrequests WHERE request_receiver='"+param+"'";
+
+						    	if(sort){
+						    		sqlGetFriends += " ORDER BY id " + sort;
+	 					    	}
+
+						    	if(limit){
+						    		sqlGetFriends += " LIMIT " + limit;
+						    	}
+
+						    	if(page){
+						    		
+						    		let skip = page == '1' ? 0 : page * limit;
+						    		sqlGetFriends += " OFFSET " + skip;
+
+						    	}
+
+						    	con.query(sqlGetFriends,(err,result)=>{
+						    				
+						    			if(!err && result.length > 0){
+						    				callback(null,{'partResultFor':result, 'fullResultFrom':arg});
+						    			}else{
+						    				callback(null,[]);
+						    			}
+										
+
+									});                            
+
+                            
+                        }, function(arg, callback) {
+						    	
+                            if(arg.partResultFor.length > 0){
+
+                                let forResult = [];
+                                var pending = arg.length;
+                             
+                                for(let i=0; i<arg.partResultFor.length; i++) {
+                                    
+                                  con.query("SELECT * FROM profiles WHERE uuid='"+arg.partResultFor[i].request_receiver+"'; SELECT * FROM users WHERE uuid='"+arg.partResultFor[i].request_receiver+"'",(err, result)=>{
+                                         
+                                         
+                                    forResult.splice(i,0,{'user':result[1],'profile':result[0]});
+                                        
+                                        if( 0 === --pending ) {
+
+                                               callback(null,{'for':forResult, 'from':arg.partResultFrom});
+
+                                        }
+
+                                    });
+                                }
+
+                            }else{
+                                callback(null, []);
+                            }
+                            
+
+                            
+                        }
+                        
+                    ], function (err, result) {
+							
+							callback(200,result);
+
+						});
+
+
+                    }else{
+
+                        callback(400,{'Error':'Token Mismatch or expired'});
+
+                    }
+            });
+
+        }else{
+
+		let errorObject = [];
+
+		if(!token){
+			errorObject.push('Token you supplied is not valid or has expired');
+		}
+		if(!uuid){
+			errorObject.push('UUID you supplied is invalid or expired');
+        }
+        
+        if(!param){
+            errorObject.push('You need to provide user uuid as a parameter');
+        }
+
+		callback(400,{'Error':errorObject});
+
+	}
 }
 
 
