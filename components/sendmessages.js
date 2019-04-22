80edd77f17bacc506b2c0478a4467fa12094ede6
Seyi Onifade <xyluz@ymail.com>
2019-04-22 19:30:24 +0100

    send message endpoint completed

--- components/sendmessages.js
+++ components/sendmessages.js
@@ -3,6 +3,8 @@ const helpers = require('./../lib/helpers');
 const uuidV1 = require('uuid/v4');
 const config = require('./../lib/config');
 const tokens = require('./../lib/tokenization');
+const async = require('async');
+const mailer = require('./mailer');
 
 const con = require('./../lib/db');
 
