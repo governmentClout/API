8a3fc92ebc3e80bbb7201ba8d9c2b447564c6096
Seyi Onifade <xyluz@ymail.com>
2019-04-29 11:56:47 +0100

    states fetch documentationa nd get

--- components/lga.js
+++ components/lga.js
@@ -12,6 +12,28 @@ states.options = (data,callback)=>{
 }
 
 
+/**
+ * @api {get} /lga/:uuid get LGAs 
+ * @apiName getLga
+ * @apiGroup LGA
+ * @apiHeader {String} uuid Authorization UUID.
+ * @apiHeader {String} Token Authorization Token.
+ * @apiDescription The endpoint deletes a petition
+ * @apiParam {String} uuid UUID of the state
+ *
+ *@apiSuccessExample Success-Response:
+ *HTTP/1.1 200 OK
+{
+    
+}
+ *@apiErrorExample Error-Response:
+ *HTTP/1.1 404 Bad Request
+{
+   
+}
+
+*/
+
 
 lga.get = (data,callback)=>{
         //return all states
--- components/states.js
+++ components/states.js
@@ -11,10 +11,127 @@ states.options = (data,callback)=>{
 	
 }
 
+/**
+ * @api {get} /states get States 
+ * @apiName getStates
+ * @apiGroup States
+ * @apiHeader {String} uuid Authorization UUID.
+ * @apiHeader {String} Token Authorization Token.
+ * @apiDescription The endpoint deletes a petition
+ * @apiParam {String} uuid UUID of the pedition
+ *
+ *@apiSuccessExample Success-Response:
+ *HTTP/1.1 200 OK
+{
+    "states": [
+        {
+            "id": 1,
+            "name": "Abia State"
+        },
+        {
+            "id": 2,
+            "name": "Adamawa State"
+        },
+        {
+            "id": 3,
+            "name": "Akwa Ibom State"
+        },
+        {
+            "id": 4,
+            "name": "Anambra State"
+        }
+        .
+        .
+        .
+        .
+        ]
+}
+ *@apiErrorExample Error-Response:
+ *HTTP/1.1 404 Bad Request
+{
+   
+}
+
+*/
 
 
 states.get = (data,callback)=>{
-        //return all states
+      
+        let user = typeof(data.headers.uuid) == 'string' && data.headers.uuid.trim().length > 0 ? data.headers.uuid.trim() : false;
+	let token = typeof(data.headers.token) == 'string' && data.headers.token.trim().length > 0 ? data.headers.token.trim() : false;
+
+        let page = typeof(data.queryStringObject.page) == 'string'  ? data.queryStringObject.page : '1'; 
+	let limit = typeof(data.queryStringObject.limit) == 'string' ? data.queryStringObject.limit : '10';
+        let sort = typeof(data.queryStringObject.sort) == 'string' && data.queryStringObject.sort.trim().length > 0 && (data.queryStringObject.sort.trim() == 'ASC' || 'DESC') ? data.queryStringObject.sort.trim() : 'DESC';
+        
+        if( 
+		token && 
+		user 
+
+		){
+
+                        let verifyToken = "SELECT token FROM " + config.db_name + ".tokens WHERE uuid='" + user + "'";
+
+			con.query(verifyToken, (err,result)=>{
+				
+				if(
+
+                                !err && 
+                                result[0] && 
+                                result[0].token == token 
+
+                                ){
+                                        let sql = "SELECT * FROM states";
+
+                                        if(sort){
+                                                sql += " ORDER BY id " + sort;
+                                        }
+
+                                        if(limit){
+                                                sql += " LIMIT " + limit;
+                                        }
+
+                                        if(page){
+                                                
+                                                let skip = page == '1' ? 0 : page * limit;
+                                                sql += " OFFSET " + skip;
+
+                                        }
+
+                                        con.query(sql,(err,result)=>{
+                                               
+                                                        if(!err && result.length > 0){
+
+                                                                callback(200,{'states':result});
+
+                                                        }else{
+                                                                console.log(err);
+                                                                callback(500,{'Error':err});
+                                                        }
+
+                                                });
+
+                                }else{
+                                        console.log(err);
+                                        callback(400,{'Error':'Token Mismatch or expired'});
+                                }
+                        });
+                        
+
+                }else{
+
+		let errorObject = [];
+
+		if(!token){
+			errorObject.push('Token you supplied is not valid or has expired');
+		}
+		if(!user){
+			errorObject.push('uuid in the header not found');
+		}
+
+		callback(400,{'Error':errorObject});
+
+	}
 }
 
 
