b293cf4a68ca4d12f76b9bb88c1f3603408e1726
Seyi Onifade <xyluz@ymail.com>
2019-07-26 15:58:15 +0100

    profile edit works

--- controllers/profiles.js
+++ controllers/profiles.js
@@ -149,10 +149,7 @@ profiles.get = (data,callback)=>{
 				//TODO: This should be optimzed
 				console.log(err);
 				callback(500,err);
-			})	;		
-
-		let headerChecker = "SELECT * FROM tokens WHERE uuid='" + uuidHeader + "'";
-		
+			})	;				
 		
 	}else{
 		let errorObject = [];
@@ -170,91 +167,67 @@ profiles.get = (data,callback)=>{
 }
 
 profiles.put = (data,callback)=>{
-	
-	let tokenHeader = data.headers.token;
-	let uuidHeader = data.headers.uuid;
-	let uuid = typeof(uuidHeader) == 'string' && uuidHeader.trim().length > 0 ? uuidHeader.trim() : false;
-	let token = typeof(tokenHeader) == 'string' && tokenHeader.trim().length > 0 ? tokenHeader.trim() : false;
 
-	if(token && uuid){
+	let uuidHeader = typeof(data.headers.uuid) == 'string' && data.headers.uuid.trim().length > 0 ? data.headers.uuid.trim() : false;
+	let tokenHeader = typeof(data.headers.token) == 'string' && data.headers.token.trim().length > 0 ? data.headers.token.trim() : false;
 
-		let verifyToken = "SELECT token FROM " + config.db_name + ".tokens WHERE uuid='" + uuid + "'";
+	if(tokenHeader && uuidHeader){
 
-		con.query(verifyToken, (err,result)=>{
-
-			if(
-				!err && 
-				result[0] && 
-				result[0].token == token 
-
-				){
-
-				
-					const checkUser = "SELECT * FROM users WHERE uuid='" + uuid + "'";
-					
-
-						con.query(checkUser,  (err,result) => {
-
-							//check if profule already exist
-							console.log(' result ' + result);
-
-							if(!err && result.length > 0){
-
-								let checkProfile = "SELECT * FROM profiles WHERE uuid='" + uuid + "'";
-
-								con.query(checkProfile, (err,result)=>{
-									// console.log(result);
-									if(!err && 
-										result.length > 0
-										){
-										let nationality_residence = typeof(data.payload.nationality_residence) == 'string' && data.payload.nationality_residence.trim().length >= 3 ? data.payload.nationality_residence.trim() : result[0].nationality_residence;
-										let nationality_origin = typeof(data.payload.nationality_origin) == 'string' && data.payload.nationality_origin.trim().length >= 3 ? data.payload.nationality_origin.trim() : result[0].nationality_origin;
-										let state = typeof(data.payload.state) == 'string' && data.payload.state.trim().length >= 2 ? data.payload.state.trim() : result[0].state;
-										let lga = typeof(data.payload.lga) == 'string' && data.payload.lga.trim().length >= 2 ? data.payload.lga.trim() : result[0].lga;
-										let firstName = typeof(data.payload.firstName) == 'string' && data.payload.firstName.trim().length > 0 ? data.payload.firstName.trim() : result[0].firstName;
-										let lastName = typeof(data.payload.lastName) == 'string' && data.payload.lastName.trim().length > 0 ? data.payload.lastName.trim() : result[0].lastName;
-										let photo = typeof(data.payload.photo) == 'string' && data.payload.photo.trim().length > 0 ? data.payload.photo.trim() : result[0].photo;
-										let background = typeof(data.payload.background) == 'string' && data.payload.background.trim().length > 0 ? data.payload.background.trim() : result[0].background;
-
-
-										let updated_at = new Date().toISOString().slice(0, 19).replace('T', ' ');
-									
-
-										let sql = "UPDATE profiles SET nationality_residence='" + nationality_residence + "', nationality_origin='" + nationality_origin + "', state ='"+state+"', lga ='"+lga+"', firstName='"+firstName+"', lastName='"+lastName+"',photo='"+ photo +"', background='"+background+"', updated_at='"+updated_at.toString()+"' WHERE uuid='" +uuid+ "'"; 
-
-										con.query(sql,  (err,result) => {
-
-										   	if(!err){
-										   		console.log(result);
-										   		
-										   		callback(200, {'Success':'Profile Update Done'});
-
-										   	}else{
-										   		console.log(err);
-										   		callback(400, {'Error':'Profile Update Failed'});
-										   		// callback(500, {'Error':'Table creation failed, its possible this table already exists'});
-										   	}
-
-								 		});
-
-									}else{
-										callback(404,{'Error':'User profile not found, please create a profile'});
-									}
-								});
-
-								
-							}else{
-								callback(404,{'Error':'User not found'});
-							}
-
-						});						
-
-
-			}else{
+		token.verify(uuidHeader,tokenHeader).then((result)=>{
+			
+			if(!result){
 				callback(400,{'Error':'Token Mismatch or expired'});
 			}
 
-		});	
+		}).then(()=>{
+
+			models.Profile.findOne({where: {userId:uuidHeader}})
+			.then((profile)=>{
+
+				console.log('foubd orifuke');
+				console.log(profile);
+
+				if(profile){
+
+					let nationalityResidence = typeof(data.payload.nationalityResidence) == 'string' && data.payload.nationalityResidence.trim().length >= 3 ? data.payload.nationalityResidence.trim() : profile.nationalityResidence;
+					let nationalityOrigin = typeof(data.payload.nationalityOrigin) == 'string' && data.payload.nationalityOrigin.trim().length >= 3 ? data.payload.nationalityOrigin.trim() : profile.nationalityOrigin;
+					let state = typeof(data.payload.state) == 'string' && data.payload.state.trim().length >= 2 ? data.payload.state.trim() : profile.state;
+					let lga = typeof(data.payload.lga) == 'string' && data.payload.lga.trim().length >= 2 ? data.payload.lga.trim() : profile.lga;
+					let firstName = typeof(data.payload.firstName) == 'string' && data.payload.firstName.trim().length > 0 ? data.payload.firstName.trim() : profile.firstName;
+					let lastName = typeof(data.payload.lastName) == 'string' && data.payload.lastName.trim().length > 0 ? data.payload.lastName.trim() : profile.lastName;
+					let photo = typeof(data.payload.photo) == 'string' && data.payload.photo.trim().length > 0 ? data.payload.photo.trim() : profile.photo;
+					let background = typeof(data.payload.background) == 'string' && data.payload.background.trim().length > 0 ? data.payload.background.trim() : profile.background;
+					// let updatedAt = new Date().toISOString().slice(0, 19).replace('T', ' ');
+
+					let dataObject = {						
+						nationalityOrigin: nationalityOrigin,
+						nationalityResidence:nationalityResidence,
+						state:state,
+						lga:lga,
+						firstName:firstName,
+						lastName:lastName,
+						photo:photo,
+						background:background
+					};
+					profile.update(dataObject)
+					.then(() => {
+						callback(200,{'Success':'Profile Update done'});
+					}).catch(err=>callback(500,err));
+					
+
+				}else{
+					callback(404,{'Error':'User Profile Does not exist, create profile instead'});
+				}				
+
+			});			
+		
+
+		}).catch((err)=>{
+			//TODO: This should be optimzed
+			console.log(err);
+			callback(500,err);
+		});		
+	
 
 	}else{
 		callback(400,{'Error':'Token Invalid or expired'});
