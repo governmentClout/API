9b4db575a5f0673a17ff5bddf771b2c63469b6f7
Seyi Onifade <xyluz@ymail.com>
2019-07-26 21:33:17 +0100

    fedrep migration

---
+++ models/fedrep.js
@@ -0,0 +1,11 @@
+'use strict';
+module.exports = (sequelize, DataTypes) => {
+  const FedRep = sequelize.define('FedRep', {
+    name: DataTypes.STRING,
+    districtId: DataTypes.INTEGER
+  }, {});
+  FedRep.associate = function(models) {
+    // associations can be defined here
+  };
+  return FedRep;
+};
